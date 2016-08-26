7ea3e758c8e82e255c25a97a49aa45324a4b74cf
Speks of Dust <speksofdust@users.noreply.github.com>
2016-08-26 16:01:21 -0400

    indexof_safe and friends no longer needed - indexof is now safe

--- src/core/include/vc.hpp
+++ src/core/include/vc.hpp
@@ -25,9 +25,6 @@ template <class ITEM_T, class CLS_T>
 class VC { // Vector container class
 protected:
     std::vector<ITEM_T> items;
-    void _del_item_safe(ITEM_T item) {
-        this._del_item_by_idx(this->indexof_safe(item));
-    }
     void _del_item(ITEM_T item) {this._del_item_by_idx(this->indexof(item));}
     void _del_item_by_idx(const unsigned i) {
         if (i == 0 || i <= this->size())
@@ -36,21 +33,16 @@ protected:
     void _add_item(ITEM_T &item) {this->items.push_back;}
 public:
     unsigned size(void) const {return this->items.size();}
-    unsigned indexof(ITEM_T item) {
-        // should only be called when item is guarenteed to exist in items
-        for (unsigned i=0; i<this->items.size(); i++)
+    long indexof(ITEM_T item) {
+        // returns -1 if item does not exist
+        const unsigned s = this->items.size();
+        for (unsigned i=0; i<s; i++)
             if (this->items[i] == item) return i;
-    }
-    unsigned indexof_safe(ITEM_T item) {
-        // same as indexof but throws error if item does not exist
-        try { return indexof(item); }
-        catch(const std::exception& e) {
-            std::cout << "item: '" << e.what() << "' does not exist in this "
-                "vector array.\n";
-        }
+        return -1;
     }
     bool has_item(ITEM_T item) {
-        for (unsigned i=0; i<this->items.size(); i++)
+        const unsigned s = this->items.size();
+        for (unsigned i=0; i<s; i++)
             if (this->items[i] == item) return true;
         return false;
     }
